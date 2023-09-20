be480b3710602c325433194997226233cf7bfeb3
Gustavo Miler <gustavo.miler@live.com>
2023-09-20 20:19:34 -0300

    Atualizando script etl

--- etl/scriptEtl.py
+++ etl/scriptEtl.py
@@ -73,7 +73,7 @@ for file in file_path:
             tabela.drop([0, 1, 2, 3], inplace=True)
             tabela.drop(tabela.columns[0], axis=1, inplace=True)
             tabela.fillna(-1, inplace=True)
-            tabela[tabela.columns[[2, 3, 4, 5, 6, 7, 8]]] = tabela[tabela.columns[[2, 3, 4, 5, 6, 7, 8]]].applymap(lambda x: x.replace(',','.') if isinstance(x, str) else x).astype(float)
+            tabela[tabela.columns[[2, 3, 4, 5, 6, 7, 8]]] = tabela[tabela.columns[[2, 3, 4, 5, 6, 7, 8]]].map(lambda x: x.replace(',','.') if isinstance(x, str) else x).astype(float)
             tabelas_nota.append(tabela)
     
     for i, tabela in enumerate(tabelas_nota):
